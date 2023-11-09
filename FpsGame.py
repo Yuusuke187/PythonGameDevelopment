348024064bd48bdf37debfa381499e9d0cfc3fe4
Paxton Perry <paxton.perry@protonmail.com>
2023-11-09 15:48:34 -0800

    Added more details about getting started

--- FpsGame.py
+++ FpsGame.py
@@ -3,4 +3,13 @@
 import pygame
 import moderngl # This still needs to be installed
 import glm # This is the OpenGL Mathematics library for Python
+import numpy
 
+# We need to run pip install moderngl without anything else running to build the library
+# Also, use this video as reference
+# https://www.youtube.com/watch?v=yJXuvK_eLrQ
+
+# The file basetsd.h is missing - The Windows 10 SDK needs to be updated for this file
+# Also, wherever rc.exe is needs to be added to PATH in the Environment Variables
+
+# Download Tiled at www.mapeditor.org to get a level editor
